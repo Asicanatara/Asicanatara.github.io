515fb1a7c8c6ae30b8f18fa2c8e778baf10b0ad0
Asicanatara <129828129+Asicanatara@users.noreply.github.com>
2024-05-08 16:50:34 +1000

    w6

--- Week 5/script.js
+++ Week 5/script.js
@@ -41,10 +41,10 @@ else if (tempInput<= 10) {
 
 let shoppingCart =
 [
-    {name:"T-shirt", price: 20}
-    {name:"Jeans", price: 50}
-    {name:"Sneakers", price: 80}
-    {name:"Backpack", price: 30}
+    {name:"T-shirt", price: 20},
+    {name:"Jeans", price: 50},
+    {name:"Sneakers", price: 80},
+    {name:"Backpack", price: 30},
 ]
 
 let total = 0;
