644ed5b64806fc95fb0b0d465965b56995b52a15
Asicanatara <129828129+Asicanatara@users.noreply.github.com>
2024-05-16 00:17:53 +1000

    Oxie mods

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,3 +1,39 @@
-const airportAudio = document.querySelector("#battlecry");
-console.log(battleCry);
-// airportAudio
+document.addEventListener("DOMContentLoaded", function (event) {
+    // Get elements
+    const $ = (s) => document.querySelector(s);
+    const container = $(".media-player");
+    const videoElement = $("video");
+    const largePlay = $(".large-circle-button");
+  
+    console.log(container);
+  
+    largePlay.onclick = () => {
+      // Start the video
+  
+      videoElement.play();
+  
+      // Update the UI
+  
+      container.setAttribute("is-playing", "true");
+      videoElement.setAttribute("controls", "true");
+  
+      // Bind keyboard shortcuts after we play, controls dont exist yet
+  
+      window.addEventListener("keydown", (e) => {
+  
+        // [f] Toggle fullscreen
+        if (e.key.toLowerCase() == "f") {
+          if (document.fullscreenElement) {
+            document.exitFullscreen();
+          } else {
+            videoElement.requestFullscreen();
+          }
+        }
+  
+        // [m] Toggle mute
+        if (e.key.toLowerCase() == "m") {
+          videoElement.muted = !videoElement.muted;
+        }
+      });
+    };
+  });
