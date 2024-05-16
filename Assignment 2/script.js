f848457c2ae65115b51a3e7b071cd4de59069fb2
Asicanatara <129828129+Asicanatara@users.noreply.github.com>
2024-05-16 10:01:31 +1000

    updated UX

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,39 +1,44 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    // Getting the elements
-    const $ = (s) => document.querySelector(s);
-    const container = $(".media-player");
-    const videoElement = $("video");
-    const largePlay = $(".large-circle-button");
-  
-    console.log(container);
-  
-    largePlay.onclick = () => {
-      // Start the video
-  
-      videoElement.play();
-  
-      // Update the UI
-  
-      container.setAttribute("is-playing", "true");
-      videoElement.setAttribute("controls", "true");
-  
-      // Bind keyboard shortcuts after we play, controls dont exist yet
-  
-      window.addEventListener("keydown", (e) => {
-  
-        // [f] Toggle fullscreen
-        if (e.key.toLowerCase() == "f") {
-          if (document.fullscreenElement) {
-            document.exitFullscreen();
-          } else {
-            videoElement.requestFullscreen();
-          }
+  // Getting the elements
+  const $ = (s) => document.querySelector(s);
+  const container = $(".media-player");
+  const videoElement = $("video");
+  const largePlay = $(".large-circle-button");
+
+  console.log(container);
+
+  largePlay.onclick = () => {
+    // Start the video
+
+    videoElement.play();
+
+    // Update the UI
+
+    container.setAttribute("is-playing", "true");
+    videoElement.setAttribute("controls", "true");
+
+    // Bind keyboard shortcuts after we play, controls dont exist yet
+
+    window.addEventListener("keydown", (e) => {
+      // [f] Toggle fullscreen
+      if (e.key.toLowerCase() == "f") {
+        if (document.fullscreenElement) {
+          document.exitFullscreen();
+        } else {
+          videoElement.requestFullscreen();
         }
-  
-        // [m] Toggle mute
-        if (e.key.toLowerCase() == "m") {
-          videoElement.muted = !videoElement.muted;
-        }
-      });
-    };
-  });
+      }
+
+      // [m] Toggle mute
+      if (e.key.toLowerCase() == "m") {
+        videoElement.muted = !videoElement.muted;
+      }
+    });
+  };
+});
+
+const box = document.getElementById("expandableBox");
+
+box.addEventListener("click", function () {
+  this.classList.toggle("expand");
+});
